2fcc6b394f5b876b1b2b5aaf91b6937ee4246cd5
Kat Lau <katinthehat131@gmail.com>
2011-12-20 02:30:56 -0500

    updated html, css, started working on js

--- public/main.js
+++ public/main.js
@@ -1,61 +1,4 @@
 $(document).ready(function(){
-    
-    //what happens when you click the new crush button?
-    $('#new_post').click(insertNewText);
-    
-    //what happens when you click the submit post button?
-    $('#submit_text').click(submitText);
-    
-    
-    //---This code handles the default placeholder text
-    $('.defaultText').live('focus', function(){
-        if ( $(this).val() === $(this).attr("alt") )
-           $(this).val("");
-    });  
-    $('.defaultText').live('blur', function(){
-        if ( $(this).val() === "" )
-            $(this).val( $(this).attr("alt") );
-    });
-    //-- End placeholder code
-    
-});
-
-
-//Our insert post function
-function insertNewText(){
-    //hide the button and show our new post box
-    $('#new_post').hide();
-    $('#new_text_box').show();
-}
-
-
-//Our submit post function
-function submitText(){
-    
-   //clone an existing post, and set values of the new post
-   var $newpost = $('.text').first().clone();
-   $newpost.find(".text_body").html( $("#new_text_body").val() );
-   $newpost.find(".text_time").html( "just now.");
-   
-   //insert the new post in the page
-   $('#new_text_box').after($newpost);
-   
-   //then hide the new_post box and re show the new button
-   $('#new_text_box').hide();
-   $('#new_post').show();
-
-}
-
-
-
-
-
-
-
-
-
-
-$(document).ready(function(){
 		$( "#atrocityslider" ).slider({
 			value:50,
 			min: 0,
@@ -67,18 +10,78 @@ $(document).ready(function(){
 		});
 		$( "#atrocityamount" ).val( $( "#atrocityslider" ).slider( "value" ) );
 	});
-	
-	
+
+//what displays when you click each link?
 
 $(document).ready(function(){
-		$( "#currentslider" ).slider({
-			value:5,
-			min: 0,
-			max: 100,
-			step: 5,
-			slide: function( event, ui ) {
-				$( "#currentamount" ).val(  ui.value );
-			}
-		});
-		$( "#currentamount" ).val( $( "#currentslider" ).slider( "value" ) );
-	});
+    
+	$('#submitbutton').click(tooSoon);
+	$('#submitbutton').click(showResults);
+	$('#sneakLink').click(showSneak);
+	$('#backHome').click(goBack);
+    
+});
+
+
+//functions determining window displays
+function showResults(){
+    $('#toosoontest').hide();
+    $('#results').show();
+	$('#sneak').hide();
+}
+
+function showSneak(){
+	$('#toosoontest').hide();
+	$('#results').hide();
+	$('#sneak').show();
+}
+
+function goBack(){
+	$('#toosoontest').show();
+	$('#results').hide();
+	$('#sneak').hide();
+}
+
+//create variables for Too Soon Test
+var about = $('#about').val();
+var amount = $('#amount').val();
+var livesAffected = $('#livesAffected').val();
+var separation = $('#separation').val();
+var timeSince = $('#timeSince').val();
+
+//Too Soon Test
+//turns out you can't just multiply/divide variables or anything...hmmmmm
+//var result = ( atrocityamount * livesAffected )/(separation * timeSince)
+//function tooSoon(atrocityamount, livesAffected, separation, timeSince){
+
+
+//}
+
+
+
+//take values submitted from form and enter into algorithm
+//   var $newpost = $('.text').first().clone();
+//   $newpost.find(".text_body").html( $("#new_text_body").val() );
+//   $newpost.find(".text_time").html( "just now.");
+   
+   //insert the new post in the page
+//   $('#new_text_box').after($newpost);
+   
+   //then hide the new_post box and re show the new button
+//   $('#new_text_box').hide();
+//   $('#new_post').show();
+
+if (result >= 1)
+{
+//code to be executed if the conditions are met	
+	document.write("Congratulations! With a Too Soon Quotient of " + result + ", your joke about " + about + "was just soon enough. Keep those jokes comin'!");
+}
+else {
+//code to execute if condition is not true	
+	document.write("Woah there. With a Too Soon Quotient of " + result + ", your joke was a bit too soon. Give it some time.");
+}
+
+
+
+
+
