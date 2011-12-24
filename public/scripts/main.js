e87b3a5a76e7c6df45dad4d1b9c0bc0cee6b5d71
Kat Lau <katinthehat131@gmail.com>
2011-12-23 23:39:15 -0500

    updates

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -9,53 +9,78 @@ $(document).ready(function(){
 			}
 		});
 		$( "#atrocityamount" ).val( $( "#atrocityslider" ).slider( "value" ) );
-	});
+});
+
+//what happens when we click?
+$('#submitbutton').click(testSubmit);
+
+function testSubmit(){
+	$('#toosoontest').hide();
+	$('#results').show();
+	$('#sneak').hide();
+	runTooSoonTest();
+}
 
 //create variables for Too Soon Test
+function runTooSoonTest(){
+	if ($('#separation').val() === "kevinbacon")
+		{var separation = 7;}
+	else {var separation = $('#separation').val();}
 
-if ($('#separation').val() === "kevinbacon")
-	{var separation = 7}
-	else {var separation = $('#separation').val();
-	}
 //is this even a valid way to make this condition? sort of just made this up...slash testing it in the console isn't turning kevinbacon into a 7.
 
-if ($('.choose').val() === "yes")
-	{var laugh = 2}
+	if ($('.choose').val() === "yes")
+		{var laugh = 2;}
 	else if ($('.choose').val() === "no")
-	{var laugh = 0}
+		{var laugh = 0;}
 	else if ($('.choose').val() === "me")
-	{var laugh = 1}
+		{var laugh = 1;}
 
-if ($('.choose').val() === "nazis")
-	{var groups = 3}
+	if ($('.choose').val() === "nazis")
+		{var groups = 3;}
 	else if ($('.choose').val() === "asians")
-	{var groups = 1}
+		{var groups = 1;}
 	else if ($('.choose').val() === "kim")
-	{var groups = 1}
+		{var groups = 1;}
 	else if ($('.choose').val() === "occupy")
-	{var groups = 2}
+		{var groups = 2;}
 	//need to figure out how to deal with someone who checks multiple groups...
 
-var atrocityamount = $('#atrocityamount').val();
-var about = $('#about').val();
-var amount = $('#amount').val();
-var livesAffected = $('#livesAffected').val();
-var separation = $('#separation').val();
-var timeSince = $('#timeSince').val();
+	var atrocityamount = $('#atrocityamount').val();
+	var about = $('#about').val();
+	var amount = $('#amount').val();
+	var livesAffected = $('#livesAffected').val();
+	var separation = $('#separation').val();
+	var timeSince = $('#timeSince').val();
 
-var result = ( atrocityamount * (livesAffected * 0.4))/(separation * timeSince * laugh)
+	var result = ( atrocityamount * (livesAffected * 0.4))/(separation * timeSince * laugh);
 //this is a placeholder. actual formula will be different.
 
-
-if (result <= 1)
-{
-	$('#result').html("Congratulations! With a Too Soon Quotient of " + result + ", your joke about " + about + "was just soon enough. Keep those jokes comin'!");
+	if (result <= 1)
+	{
+	$('#result').html("Congratulations! With a Too Soon Quotient of " + result + ", your joke about " + about + "was just soon enough. Keep those jokes comin! <br /><span id='sneaklink'>Click here</span> to see how we calculated...");
+	}
+	else 
+	{
+	$('#result').html("Woah there. With a Too Soon Quotient of " + result + ", your joke was a bit too soon. Give it some time. <br /><span id='sneaklink'>Click here</span> to see how we calculated...");
+	}
 }
-else {
-	$('#result').html("Woah there. With a Too Soon Quotient of " + result + ", your joke was a bit too soon. Give it some time.");
+
+//functions for other pages
+$('#sneaklink').click(showSneak);
+$('#homelink').click(goHome);
+
+function showSneak(){
+	$('#toosoontest').hide();
+	$('#results').hide();
+	$('#sneak').show();
 }
 
-
+function goHome(){
+	$('#toosoontest').show();
+	$('#results').hide();
+	$('#sneak').hide();
+}
 
 
 
